dbb419f770738b292fa6a819f26eb044df8f6844
KaiserHua1901 <KaiserHua1901@outlook.com>
2023-07-17 11:17:58 -0700

    just test out path follow node

--- Scene/Enemy.gd
+++ Scene/Enemy.gd
@@ -4,10 +4,10 @@ extends CharacterBody2D
 @export var move_speed = 30
 @onready var player = get_tree().get_first_node_in_group("player")
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * move_speed
-	move_and_slide()
-#	move_and_collide(velocity * delta)
+	#move_and_slide()
+	move_and_collide(velocity * delta)
 	print(player)
 	print(direction)
--- Scene/playground.gd
+++ Scene/playground.gd
@@ -1,3 +1,17 @@
 extends Node2D
 
-
+#var enemy = preload("res://Scene/Enemy.tscn")
+#
+#func _ready():
+#	randomize()
+#
+#func _on_timer_timeout():
+#	var rng = RandomNumberGenerator.new()
+#	rng.randomize()
+#
+#
+#	$player/SpawnEnemy/SpawnLocation.offset = rng.randi_range(0,3000)
+#
+#	var EnemyIns = enemy.instantiate()
+#
+#	instance.global_position = $
